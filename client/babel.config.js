08c2b7cbd24fe99212b197f1dddd16e029756eab
Vladislav Epifanov <v.epifanow2012@gmail.com>
2025-03-18 00:15:15 +0300

    upd conf

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     presets: [
       ['@babel/preset-env', { targets: { node: 'current' } }],
-      ['@babel/preset-react', { runtime: 'automatic' }],
+      ['@babel/preset-react', { 
+        runtime: 'automatic',
+        importSource: '@emotion/react' // Добавляем поддержку Emotion для MUI
+      }],
       '@babel/preset-typescript'
-    ]
+    ],
+    plugins: ['@emotion/babel-plugin'] // Важно для корректной работы MUI
   };
