fd8605fd4234aaf2dd89196d01e30e4ed3d8ec92
Vladislav Epifanov <v.epifanow2012@gmail.com>
2025-03-17 23:43:04 +0300

    upd

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen, waitFor, fireEvent } from '@testing-library/react'
 import axios from 'axios'
-import App from './App.tsx'
+import App from './App'
 import '@testing-library/jest-dom'
 
 jest.mock('axios')
