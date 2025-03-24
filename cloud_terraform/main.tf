830889dd865bd53fe49aaf8363a119337297ff57
Vladislav Epifanov <v.epifanow2012@gmail.com>
2025-03-25 01:41:39 +0300

    terraform file

---
+++ cloud_terraform/main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "ru-central1-a"
+}
+
+resource "yandex_compute_disk" "boot-disk" {
+  name     = "boot-disk"
+  type     = "network-hdd"
+  zone     = "ru-central1-a"
+  size     = "20"
+  image_id = "fd86cpunl4kkspv0u25a"
+}
+
+resource "yandex_compute_instance" "vm" {
+  name = "vm"
+
+  resources {
+    cores  = 4
+    memory = 4
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk.id
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("C:/Users/vepif/.ssh/yandex_cloud.pub")}"
+  }
+}
+
+resource "yandex_vpc_network" "network" {
+  name = "network"
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "subnet"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm" {
+  value = yandex_compute_instance.vm.network_interface.0.ip_address
+}
+
+
+output "external_ip_address_vm" {
+  value = yandex_compute_instance.vm.network_interface.0.nat_ip_address
+}
