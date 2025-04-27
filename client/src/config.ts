91070228546fc7660881f59079a4884e09cf0f79
Vladislav Epifanov <v.epifanow2012@gmail.com>
2025-04-27 19:26:15 +0300

    upd conf

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,10 @@
 export function getApiUrl(): string {
     if (process.env.NODE_ENV === 'test') {
-      return "http://localhost:8000"; // Для тестов
+      return "http://localhost:8000";
     }
-    return import.meta.env.VITE_API_URL || "http://localhost:8000";
+    if (typeof window !== "undefined") {
+      return import.meta.env.VITE_API_URL || "http://localhost:8000";
+    }
+    return "http://localhost:8000";
   }
   
